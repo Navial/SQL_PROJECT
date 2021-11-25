697482a543778640546e44e491cb9bd5c329708d
alexnightborn <alexandre.walwis@nightborn.be>
2021-11-25 15:28:31 +0100

    fix procedure 1

--- script/script.sql
+++ script/script.sql
@@ -49,36 +49,38 @@ VALUES ('Denis',
 SELECT id_etudiant, nom, prenom, bloc, email, mot_de_passe FROM projet_sql.etudiants WHERE email = 'victor.denis@gmail.com';
 
 -- procedure 1
-/*CREATE OR REPLACE FUNCTION projet_sql.ajouter_UE (
+CREATE OR REPLACE FUNCTION projet_sql.ajouter_UE (
     projet_sql.ues.code_ue%TYPE,
     projet_sql.etudiants.id_etudiant%TYPE)
     RETURNS projet_sql.ues.code_ue%TYPE
 AS $$
 DECLARE
-    code_ue_param ALIAS FOR $1;
+code_ue_param ALIAS FOR $1;
     id_etudiant_param ALIAS FOR $2;
-    id_pae INTEGER := 0;
+    id_pae_var INTEGER := 0;
     pae_est_valide BOOLEAN := 0;
     nb_credits_valides INTEGER := 0;
     id_ue INTEGER := 0;
 BEGIN
-    id_pae := (SELECT id_pae FROM projet_sql.paes WHERE id_etudiant = id_etudiant_param);
+    id_pae_var := (SELECT id_pae FROM projet_sql.paes WHERE id_etudiant = id_etudiant_param);
     pae_est_valide := (SELECT est_valide FROM projet_sql.paes WHERE id_etudiant = id_etudiant_param);
     nb_credits_valides := (SELECT sum(nombre_credits) FROM ues u, ues_valide v WHERE u.code_ue = code_ue_param AND u.code_ue = v.code_ue AND v.id_etudiant = id_etudiant_param);
     IF pae_est_valide THEN
         RAISE 'Votre PAE est deja valide';
-        IF EXISTS(SELECT id_etudiant, code_ue FROM projet_sql.ues_valide WHERE id_etudiant = id_etudiant_param AND code_ue = code_ue_param) THEN
-            RAISE 'Cette UE est deja valide';
-            IF EXISTS(SELECT ue_prerequise
-                      FROM projet_sql.acces_ue
-                      WHERE ue_suivante = code_ue_param
-                        AND ue_prerequise NOT IN (SELECT code_ue FROM ues_valide WHERE id_etudiant=id_etudiant_param)) THEN
-                RAISE 'Cette UE a des prerequis a valider';
-                IF (nb_credits_valides < 30) AND (SELECT num_bloc FROM blocs b, ues u WHERE b.num_bloc = u.num_bloc AND u.code_ue = code_ue_param) <> 1 THEN
-                    RAISE 'Moins de 30 credits ont ete valides, vous ne pouvez que choisir une UE du bloc 1';
-                    INSERT INTO projet_sql.lignes_ue VALUES(DEFAULT, code_ue_param, @id_pae) RETURNING code_ue INTO id_ue;
-
-                    RETURN id_ue;
-                END ;
+END IF;
+    IF EXISTS(SELECT id_etudiant, code_ue FROM projet_sql.ues_valide WHERE id_etudiant = id_etudiant_param AND code_ue = code_ue_param) THEN
+        RAISE 'Cette UE est deja valide';
+END IF;
+    IF EXISTS   (SELECT ue_prerequise
+                FROM projet_sql.acces_ue
+                WHERE ue_suivante = code_ue_param
+                AND ue_prerequise NOT IN (SELECT code_ue FROM ues_valide WHERE id_etudiant=id_etudiant_param)) THEN
+        RAISE 'Cette UE a des prerequis a valider';
+END IF;
+    IF (nb_credits_valides < 30) AND (SELECT num_bloc FROM blocs b, ues u WHERE b.num_bloc = u.num_bloc AND u.code_ue = code_ue_param) <> 1 THEN
+        RAISE 'Moins de 30 credits ont ete valides, vous ne pouvez que choisir une UE du bloc 1';
+END IF;
+INSERT INTO projet_sql.lignes_ue VALUES(DEFAULT, code_ue_param, id_pae_var) RETURNING code_ue INTO id_ue;
+RETURN id_ue;
+END ;
 $$ LANGUAGE plpgsql;
-*/
